bfd4525d9af8f0dfa910ae55e41c582f30616519
Matthias Treydte <mt@waldheinz.de>
2014-03-20 15:21:06 +0100

    Introduced an newtype for wrapping CHK extra bytes.

--- src/Freenet/URI.hs
+++ src/Freenet/URI.hs
@@ -17,11 +17,13 @@ import Freenet.Types
 
 data URI
      = CHK
-       { chkLocation :: Key           -- ^ the routing key
-       , chkKey      :: Key           -- ^ the crypto key
-       , chkExtra    :: BS.ByteString -- ^ extra data about algorithms used, always 5 bytes
+       { chkLocation :: Key      -- ^ the routing key
+       , chkKey      :: Key      -- ^ the crypto key
+       , chkExtra    :: ChkExtra -- ^ extra data about algorithms used, always 5 bytes
        }
-     deriving ( Show )
+
+instance Show URI where
+  show (CHK l k e) = "CHK@" ++ show l ++ "," ++ show k ++ "," ++ show e
 
 parseUri :: T.Text -> Either T.Text URI
 parseUri str = case T.take 4 str of
@@ -36,24 +38,35 @@ parseChk str = case T.split (== ',') str of
     e <- fromBase64' estr >>= \eb -> if BS.length eb == 5
                                      then Right $ eb
                                      else Left "CHK extra data must be 5 bytes"
-    return $ CHK rk ck e
+    return $ CHK rk ck (ChkExtra e)
   _ -> Left $ T.concat $ ["expected 3 comma-separated parts in \"", str, "\""]
-  
+
 toDataRequest :: URI -> DataRequest
-toDataRequest (CHK loc _ e) = ChkRequest loc $ BS.index e 1
+toDataRequest (CHK loc _ e) = ChkRequest loc $ chkExtraCrypto e
 
 uriLocation :: URI -> Key
 uriLocation (CHK loc _ _) = loc
 
+--------------------------------------------------------------------------------------
+-- CHK extra data (last URI component)
+--------------------------------------------------------------------------------------
+
+newtype ChkExtra = ChkExtra { unChkExtra :: BS.ByteString }
+
+instance Show ChkExtra where
+  show (ChkExtra bs) = T.unpack $ toBase64' bs
 
 -- | construct CHK extra data
 mkChkExtra
   :: Word8         -- ^ crypto algorithm
   -> Word16        -- ^ comptression algorithm
   -> Bool          -- ^ control document
-  -> BS.ByteString -- ^ resulting 5 bytes of CHK key "extra" data
-mkChkExtra crypt compr contr = BSL.toStrict $
+  -> ChkExtra      -- ^ resulting 5 bytes of CHK key "extra" data
+mkChkExtra crypt compr contr = ChkExtra $ BSL.toStrict $
   BSB.toLazyByteString $ BSB.word8 0 <> BSB.word8 crypt <> BSB.word8 ctrl <> BSB.word16BE compr 
   where
     ctrl = if contr then 2 else 1
-    
+
+-- | extract the crypto algorithm used from an ChkExtra
+chkExtraCrypto :: ChkExtra -> Word8
+chkExtraCrypto ce = BS.index (unChkExtra ce) 1
