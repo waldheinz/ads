d32ceb6c9dc6dc5d26b85024f5bd3122ec822d11
Matthias Treydte <mt@waldheinz.de>
2014-03-23 18:05:48 +0100

    Turned base64 de/encoding into a typeclass and added support for Integers.

--- src/Freenet/Base64.hs
+++ src/Freenet/Base64.hs
@@ -1,15 +1,23 @@
 
 -- | Freenet's modified Base64 encoding
 module Freenet.Base64 (
-   fromBase64', toBase64'
+  FreenetBase64(..)
 ) where
 
+import Data.Bits ( shiftL, shiftR )
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy.Builder as BLB
 import Data.ByteString.Base64
+import Data.Monoid ( (<>) )
 import qualified Data.Text as T
 import Data.Word
+
+-- |
+-- things that have a special base64 representation in Freenet
+class FreenetBase64 a where
+  fromBase64' :: T.Text -> Either T.Text a
+  toBase64'   :: a -> T.Text
 
 toStd :: Word8 -> Word8
 toStd x
@@ -26,8 +34,8 @@ fromStd x
 toStandardAlphabet :: B.ByteString -> B.ByteString
 toStandardAlphabet = B.map toStd
 
-fromBase64' :: T.Text -> Either T.Text B.ByteString
-fromBase64' s = case dec of
+fromBase64Bs :: T.Text -> Either T.Text B.ByteString
+fromBase64Bs s = case dec of
   Left e   -> Left $ T.pack e
   Right b -> Right b
   where
@@ -41,6 +49,53 @@ fromBase64' s = case dec of
       | otherwise = fromIntegral $ 4 - (T.length s `rem` 4)
 
 -- | convert bytes to Freenet's modified base64
-toBase64' :: B.ByteString -> T.Text
-toBase64' b = T.takeWhile (/= '=') $
+toBase64Bs :: B.ByteString -> T.Text
+toBase64Bs b = T.takeWhile (/= '=') $
   T.map fromStd $ T.pack $ map (toEnum . fromEnum) $ B.unpack $ encode b
+
+instance FreenetBase64 B.ByteString where
+  fromBase64' = fromBase64Bs
+  toBase64'   = toBase64Bs
+
+------------------------------------------------------------------------
+-- base64 encoded integers
+------------------------------------------------------------------------
+
+bs2i :: B.ByteString -> Integer
+bs2i b
+   | B.length b' == 0 = 0
+   | sign = go b' - 2 ^ (B.length b' * 8)
+   | otherwise = go b'
+   where
+      b' = b -- B.dropWhile (==0) b -- drop leading 0s
+      sign = B.index b' 0 > 127
+      go = B.foldl' (\i bb -> (i `shiftL` 8) + fromIntegral bb) 0 
+
+i2bs :: Integer -> B.ByteString
+i2bs x
+   | x == 0 = B.singleton 0
+   | x < 0 = (\xx -> B.reverse $ B.unfoldr go xx) $ 2 ^ (8 * bytes) + x
+   | otherwise = if B.index posRes 0 > 127 then B.singleton 0 <> posRes else posRes
+   where
+      posRes = B.reverse $ B.unfoldr go x
+      bytes = (integerLogBase 2 (abs x) + 1) `quot` 8 + 1
+      go i = if i == 0 then Nothing
+                       else Just (fromIntegral i, i `shiftR` 8)
+
+-- Compute the (floor of the) log of i in base b.
+-- Simplest way would be just divide i by b until it's smaller then b,
+-- but that would be very slow!  We are just slightly more clever.
+integerLogBase :: Integer -> Integer -> Int
+integerLogBase b i =
+     if i < b then
+        0
+     else
+        -- Try squaring the base first to cut down the number of divisions.
+        let l = 2 * integerLogBase (b*b) i
+            doDiv :: Integer -> Int -> Int
+            doDiv ii ll = if ii < b then ll else doDiv (ii `div` b) (ll+1)
+        in  doDiv (i `div` (b^l)) l
+
+instance FreenetBase64 Integer where
+  fromBase64' s = fromBase64' s >>= \bs -> Right (bs2i bs)
+  toBase64'     = toBase64' . i2bs
