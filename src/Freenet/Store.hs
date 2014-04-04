7dbbcb27d1ffdc1b5b19c39b1f6e7e7e6f35047e
Matthias Treydte <mt@waldheinz.de>
2014-04-04 22:21:48 +0200

    Don't write data to store if it's already there.

--- src/Freenet/Store.hs
+++ src/Freenet/Store.hs
@@ -9,6 +9,7 @@ module Freenet.Store (
   ) where
 
 import qualified Control.Concurrent.Lock as Lock
+import Control.Monad ( void )
 import Data.Binary
 import Data.Binary.Get ( runGetOrFail )
 import Data.Binary.Put ( runPut )
@@ -54,29 +55,37 @@ locOffsets sf loc = map (\i -> (fromIntegral i `rem` (fromIntegral count)) * (fr
     entrySize = sfEntrySize sf
     
 putData :: StorePersistable f => StoreFile f -> f -> IO ()
-putData sf df = Lock.with (sfLock sf) $ doWrite where
+putData sf df = void $ putData' sf df
+
+putData' :: StorePersistable f => StoreFile f -> f -> IO (Maybe f)
+putData' sf df = Lock.with (sfLock sf) $ doWrite where
   handle = sfHandle sf
   doPut  = putWord8 1 >> storePut df
-  isFree = getWord8 >>= (\flags -> return $ flags == 0)
+  loc = dataBlockLocation df
 
-  doWrite = go offsets where
-      loc       = dataBlockLocation df
-      offsets   = locOffsets sf loc
-      go []     = logI "no free slots in store"
+  doGet = do
+    flags <- getWord8
+    if flags == 1
+      then storeGet
+      else fail "empty slot"
+
+  writeAt o = do
+    hSeek handle AbsoluteSeek o
+    BSL.hPut handle $ runPut doPut
+    logI $ (show loc) ++ " written at " ++ show o
+
+  doWrite = go $ locOffsets sf loc where
+      go []     = logI "no free slots in store" >> return Nothing
       go (o:os) = do
         hSeek handle AbsoluteSeek o
-        d <- BSL.hGet handle 1
-        case runGetOrFail isFree d of
-          Right (_, _, free) -> do
-            if free
-              then do
-                hSeek handle AbsoluteSeek o
-                BSL.hPut handle $ runPut doPut
-                print $ "written at " ++ show o
-              else go os
-          Left (_, _, e) -> error e
+        d <- BSL.hGet handle $ sfEntrySize sf
+       
+        case runGetOrFail doGet d of
+          Left  (_, _, _ ) -> writeAt o >> return Nothing
+          Right (_, _, df') -> if loc == dataBlockLocation df'
+                              then logI ((show loc) ++ " already in store") >> (return $ Just df')
+                              else go os
   
-
 getData :: StorePersistable f => StoreFile f -> Key -> IO (Maybe f)
 getData fs key = Lock.with (sfLock fs) $ doRead key where
   handle = sfHandle fs
@@ -98,4 +107,3 @@ getData fs key = Lock.with (sfLock fs) $ doRead key where
         Right (_, _, df) -> if loc == dataBlockLocation df
                             then return $ Just df
                             else go os
-    
