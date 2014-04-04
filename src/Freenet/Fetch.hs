cc6afad5316a3639ba58b4767e310499db90ace7
Matthias Treydte <mt@waldheinz.de>
2014-04-04 16:14:02 +0200

    Cleaned up Fetch module.

--- src/Freenet/Fetch.hs
+++ src/Freenet/Fetch.hs
@@ -68,14 +68,10 @@ fetchUri fn uri = do
   case db of
     Left e  -> return $ Left e
     Right plaintext -> if isControlDocument uri
-                       then do
-                         print "fetchUri: metadata"
-                         case parseMetadata plaintext of
-                           Left e   -> return $ Left e
-                           Right md -> resolvePath fn (uriPath uri) md Nothing
-                       else print "fetchUri: direct" >> (return $ Right plaintext)
-
-
+                       then case parseMetadata plaintext of
+                         Left e   -> return $ Left e
+                         Right md -> resolvePath fn (uriPath uri) md Nothing
+                       else return $ Right plaintext
 
 fetchUris :: Show a => Node a -> [URI] -> IO [(URI, Either T.Text BSL.ByteString)]
 fetchUris fn uris = do
@@ -85,13 +81,9 @@ fetchUris fn uris = do
 fetchRedirect :: Show a => Node a -> RedirectTarget -> [T.Text] -> IO (Either T.Text BSL.ByteString)
 fetchRedirect fn (RedirectKey _ uri) path = do
   logI $ "fetch redirect to " ++ show uri ++ " with path " ++ show path
---   requestNodeData fn uri
   fetchUri fn $ appendUriPath uri path
---  if isControlDocument uri
---  then fetchUri fn $ appendUriPath uri path
---  else requestNodeData fn uri
   
-fetchRedirect fn (SplitFile comp dlen olen segs _) _ = do -- TODO: we're not returning the MIME
+fetchRedirect fn (SplitFile comp dlen _ segs _) _ = do -- TODO: we're not returning the MIME and ignoring the original length
   logI $ "fetch split file"
   
   let
@@ -115,9 +107,9 @@ fetchRedirect' fn (RedirectKey _ uri) = do
   case mbs of
     Left e   -> return $ Left $ "fetchRedirect': error with requestNodeData: " `T.append` e
     Right bs -> case parseMetadata bs of
-      Left e'  -> return $ Right bs  -- return $ Left $ "fetchRedirect': can't parse metadata: " `T.append` e'
+      Left _  -> return $ Right bs  -- return $ Left $ "fetchRedirect': can't parse metadata: " `T.append` e'
       Right md -> case md of
-        ArchiveManifest sf@(SplitFile{}) atype _ _ -> fetchRedirect fn sf []
+        ArchiveManifest sf@(SplitFile{}) _ _ _ -> fetchRedirect fn sf []
         _ -> return $ Left $ "fetchRedirect': what shall I do with metadata: " `T.append` (T.pack $ show md)
 
 fetchRedirect' fn sf@(SplitFile _ _ _ _ _) = fetchRedirect fn sf []
@@ -139,16 +131,6 @@ fetchArchive fn tgt tp = do
                  _                    -> m
              ) Map.empty (const Map.empty) $ TAR.read bs
       x   -> return $ Left $ T.pack $ "unsupported archive type " ++ show x
-{-
-resolveInArchive
-  :: Show a
-  => Node a
-  -> Archive
-  -> Metadata
-  -> [T.Text]
-  -> IO (Either T.Text BSL.ByteString)
-resolveInArchive _ arch md ps = return $ Left $ T.pack $ "resolveInArchvie: " ++ show ps ++ "   " ++ show arch ++ "(md=
-  -}
     
 -- | FIXME: watch out for infinite redirects
 resolvePath
@@ -162,7 +144,7 @@ resolvePath
 resolvePath fn [] md@(Manifest _) arch = logI "redirecting to default entry" >> resolvePath fn [""] md arch
 
 -- resolve in archive
-resolvePath fn _ (ArchiveInternalRedirect tgt _) (Just amap) = do
+resolvePath _ _ (ArchiveInternalRedirect tgt _) (Just amap) = do
   logI $ "resolving AIR to " ++ show tgt
   case Map.lookup (T.unpack tgt) amap of
     Nothing -> return $ Left $ "could not locate \"" `T.append` tgt `T.append` "\" in archive"
@@ -181,20 +163,13 @@ resolvePath fn (p:ps) md@(Manifest me) arch = do
   
   case lookup p me of
     Nothing -> return $ Left $ "could not locate \"" `T.append` p `T.append` "\" in manifest"
-    Just me -> case me of
+    Just me' -> case me' of
       SymbolicShortlink tgt -> do
         logI $ "following SSL to " ++ show tgt
         resolvePath fn (tgt:ps) md arch
       x                     -> resolvePath fn ps x arch
                              -- return $ Left $ T.concat ["can not resolve ", T.pack $ show x, " against a manifest"]
-{-
-resolvePath fn ps ((ArchiveManifest tgt@(RedirectKey _ uri) atype _ _)) = do
-  logI $ "looking up archive at " ++ (show uri) ++ " for " ++ show ps
-  arch <- fetchArchive fn tgt atype
-  case arch of
-    Left e -> return $ Left $ "resolvePath (archive manifest): " `T.append` e
-    Right a -> resolveInArchive fn a ps
-  -}
+
 resolvePath fn ps (ArchiveManifest atgt atype _ _) _ = do
   logI $ "resolving archive manifest " ++ show atgt
   
@@ -214,33 +189,3 @@ resolvePath fn ps (ArchiveManifest atgt atype _ _) _ = do
 -- give up resolving this path
 resolvePath _ ps md arch = return $ Left $ T.concat [
   "cannot locate ", T.pack (show ps), " in ", T.pack (show md), " with archive ", T.pack $ show arch]
-
-
-{-
-
-    
--- resolve inside archive 
-resolvePath fn (Just aes) [p] _ = case Map.lookup (T.unpack p) aes of
-  Nothing -> return $ Left $ "could not find inA" `T.append` p
-  Just bs -> return $ Right bs
-
-resolvePath fn _ ps (Just (ArchiveManifest tgt at _ None)) = print ("am", ps) >> do
-  arch <- fetchArchive fn tgt at
-  case arch of
-    Left e -> return $ Left e
-    Right emap -> case Map.lookup ".metadata" emap of
-      Nothing -> return $ Left $ T.pack $ "no .metadata entry found in TAR archive: " ++ show emap
-      Just mdbs -> case parseMetadata mdbs of
-        Right md -> resolvePath fn (Just emap) ps (Just md) --  set archive as new reference point
-        x        -> return $ Left $ "error parsing manifest from TAR archive: " `T.append` (T.pack $ show x)
-      
-resolvePath fn _ [] (Just (SimpleRedirect _ tgt)) = print "sr" >> fetchRedirect fn tgt
---resolvePath fn [] (Left (SymbolicShortlink tgt)) = print "ssl" >> resolvePath fn arch [tgt] m
-
-{-
-resolvePath fn ps (Right emap) = Left "emap"
-resolvePath fn arch@(Just (ae, _)) [] (ArchiveInternalRedirect tgt _) = print ("air " `T.append` tgt) >> case Map.lookup tgt ae of
-  Nothing -> print ae >> (return $ Left $ "could not find in archive: " `T.append` tgt)
-  Just bs -> return $ Right bs
--}
--}
