f25d1ca03308f1c432c972a28dcf2b3dfd3babc6
Matthias Treydte <mt@waldheinz.de>
2014-06-28 15:59:36 +0200

    Some Freenet cleanups.

--- src/lib/Freenet.hs
+++ src/lib/Freenet.hs
@@ -2,29 +2,45 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Freenet (
-  Freenet, initFn,
-  getChk, getSsk,
-  offerChk, offerSsk
+  Freenet, mkFreenet, shutdownFreenet
   ) where
 
-import Control.Concurrent ( forkIO )
-import Control.Concurrent.STM
-import Control.Monad ( void )
+import           Control.Concurrent ( forkIO )
+import           Control.Concurrent.STM
+import           Control.Monad ( void )
 import qualified Data.Configurator as CFG
 import qualified Data.Configurator.Types as CFG
 import qualified Data.Text as T
 
-import Freenet.Chk
-import qualified Freenet.Companion as FC
-import Freenet.Ssk
-import Freenet.Types
+import           Freenet.Chk
+import           Freenet.Companion
+import           Freenet.Ssk
+import Freenet.Store
+import           Freenet.Types
+import           Node
 
-data Freenet a = FN
-               { fnCompanion   :: Maybe FC.Companion
-               , fnIncomingChk :: TChan ChkBlock
-               , fnIncomingSsk :: TChan SskBlock
-               }
+data Freenet a = Freenet
+                 { fnNode      :: Node a
+                 , fnCompanion :: Maybe Companion
+                 , fnChkStore  :: StoreFile ChkBlock
+                 , fnSskStore  :: StoreFile SskBlock
+                 }
 
+mkFreenet
+  :: Node a
+  -> Maybe Companion
+  -> StoreFile ChkBlock
+  -> StoreFile SskBlock
+  -> STM (Freenet a)
+mkFreenet node comp chk ssk = do
+  return $ Freenet node comp chk ssk
+
+shutdownFreenet :: Freenet a -> IO ()
+shutdownFreenet fn = do
+  shutdownStore $ fnChkStore fn
+  shutdownStore $ fnSskStore fn
+  
+{-
 -- | initializes Freenet subsystem
 initFn :: CFG.Config -> IO (Freenet a)
 initFn cfg = do
@@ -42,57 +58,5 @@ initFn cfg = do
     Just _  -> do
       comp <- FC.initCompanion ccfg (offerChk fn) (offerSsk fn)
       return $ fn { fnCompanion = Just comp }
-  
-offerSsk :: Freenet a -> SskBlock -> IO ()
-offerSsk fn df = do
-  atomically $ writeTChan (fnIncomingSsk fn) df
-
-offerChk :: Freenet a -> ChkBlock -> IO ()
-offerChk fn df = do
-  atomically $ writeTChan (fnIncomingChk fn) df
-
-waitKeyTimeout :: DataBlock f => TChan f -> Key -> IO (TMVar (Maybe f))
-waitKeyTimeout chan loc = do
-  bucket  <- newEmptyTMVarIO
-  timeout <- registerDelay $ 30 * 1000 * 1000
-  chan'   <- atomically $ dupTChan chan
-
-  let
-    doWait = orElse
-      (readTChan chan' >>= \cf -> if dataBlockLocation cf == loc then putTMVar bucket (Just cf) else doWait)
-      (readTVar timeout >>= \to -> if to then putTMVar bucket Nothing else retry)
-
-  
-  -- wait for data or timeout
-  void $ forkIO $ atomically $ doWait
-  
-  return bucket
-
--- |
--- Tries to fetch CHK data either from the store or our
--- companion. Might block quite some time if the companion
--- is involved.
-getChk :: Freenet a -> ChkRequest -> IO (Either T.Text ChkBlock)
-getChk fn dr = case fnCompanion fn of
-  Nothing -> return $ Left "not in store, no companion"
-  Just c  -> do
-    bucket <- waitKeyTimeout (fnIncomingChk fn) (dataRequestLocation dr)
-    FC.getChk c dr
-    md <- atomically $ readTMVar bucket
-  
-    return $ case md of
-      Nothing -> Left "timeout waiting for companion"
-      Just d  -> Right d
-
-getSsk :: Freenet a -> SskRequest -> IO (Either T.Text SskBlock)
-getSsk fn dr = case fnCompanion fn of
-  Nothing -> return $ Left "not in store, no companion"
-  Just c  -> do
-    bucket <- waitKeyTimeout (fnIncomingSsk fn) (dataRequestLocation dr)
-    FC.getSsk c dr
-    md <- atomically $ readTMVar bucket
-    
-    return $ case md of
-      Nothing -> Left "timeout waiting for companion"
-      Just d  -> Right d
+  -}
 
--- src/main/Main.hs
+++ src/main/Main.hs
@@ -21,7 +21,7 @@ import           System.Random ( getStdRandom )
 
 import           Paths_ads
 
-import           Freenet as FN
+import           Freenet
 import           Freenet.Chk
 import           Freenet.Fproxy as FP
 import           Freenet.Rijndael as RD
@@ -48,8 +48,8 @@ sigHandler s = do
   infoM "main" "shutdown on sigint/sigterm"
   atomically $ writeTVar s True
   
-mkFileStore :: CFG.Config -> IO (StoreFile ChkBlock, StoreFile SskBlock)
-mkFileStore cfg = do
+mkFn :: Node a -> CFG.Config -> IO (Freenet a)
+mkFn node cfg = do
   dsdir       <- CFG.require cfg "datastore.directory"
   createDirectoryIfMissing True dsdir
   
@@ -58,8 +58,21 @@ mkFileStore cfg = do
 
   sskCount    <- CFG.require cfg "datastore.ssk-count"
   sskStore    <- mkStoreFile (dsdir </> "store-ssk") sskCount
+  
+  atomically $ mkFreenet node Nothing chkStore sskStore
 
-  return (chkStore, sskStore)
+-- |
+-- Assembles the HTTP server and starts it up.
+startHttpServer :: (PeerAddress a, JSON.ToJSON a) => CFG.Config -> Node a -> IO ()
+startHttpServer cfg node = do
+  -- start HTTP Server
+  startRestApi (CFG.subconfig "node.http" cfg) node
+      
+  -- start fproxy
+  fpport <- CFG.lookup cfg "fproxy.port" :: IO (Maybe Int)
+  case fpport of
+    Nothing -> return ()
+    Just p -> error "need resolver for fproxy" -- void $ forkIO $ Warp.run p $ FP.fproxy node
   
 main :: IO ()
 main = withSocketsDo $ do
@@ -77,6 +90,8 @@ main = withSocketsDo $ do
   void $ installHandler sigINT (Catch $ sigHandler shutdown) Nothing
   void $ installHandler sigTERM (Catch $ sigHandler shutdown) Nothing
 
+  LOG.initLogging
+
   -- load (and maybe create) configuration file
   let
     cfgFile  = appDir </> "config"
@@ -87,9 +102,6 @@ main = withSocketsDo $ do
     BSL.readFile dfile >>= BSL.writeFile cfgFile
   
   cfg <- CFG.load [CFG.Required $ appDir </> "config"]
-  
-  -- initialize logging
-  LOG.initLogging -- $ CFG.subconfig "logging" cfg
 
   -- read (maybe create) identity
   mNodeInfo <- doesFileExist infoFile >>= \e ->
@@ -103,25 +115,17 @@ main = withSocketsDo $ do
 
   let fnCfg = CFG.subconfig "freenet" cfg
   
-  -- start Freenet
-  fn <- FN.initFn fnCfg
-  (chks, ssks) <- mkFileStore fnCfg
-
-  let
-    shutdownStores :: IO ()
-    shutdownStores  = do
-      shutdownStore chks
-      shutdownStore ssks
-
   -- start our node
   case mNodeInfo of
     Nothing -> do
       logE $ "problem with " ++ infoFile
-      shutdownStores
       
     Just nodeInfo -> do
       logI $ "node identity is " ++ (show $ nodeId nodeInfo)
       node <- mkNode nodeInfo
+      fn   <- mkFn node fnCfg 
+
+      startHttpServer cfg node
       
       readPeers appDir >>= \ps ->  case ps of
         Left  e     -> logW ("error parsing peers file: " ++ e)
@@ -131,15 +135,6 @@ main = withSocketsDo $ do
 
       nodeListen (CFG.subconfig "node.listen" cfg) node
       
-      -- start HTTP Server
-      startRestApi (CFG.subconfig "node.http" cfg) node
-      
-      -- start fproxy
-      fpport <- CFG.lookup cfg "fproxy.port" :: IO (Maybe Int)
-      case fpport of
-        Nothing -> return ()
-        Just p -> error "need resolver for fproxy" -- void $ forkIO $ Warp.run p $ FP.fproxy node
-
       -- wait for shutdown
       atomically $ readTVar shutdown >>= check
-      shutdownStores
+      shutdownFreenet fn
